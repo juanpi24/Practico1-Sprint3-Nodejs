bd73c16c5fff43a331b8234374042a38ff59f7d0
Juan Pablo Millicay <juan22milli@gmail.com>
2026-04-09 12:36:13 -0300

    Middleare global para registrar solicitudes

--- app.mjs
+++ app.mjs
@@ -10,6 +10,15 @@ const app= express();
 // Usa el puerto de la variable de entorno o 3001 por defecto
 const PORT= process.env.PORT || 3005;
 
+//Middleare global para registrar solicitudes
+const loggerMiddleware= (req, res, next)=>{
+    console.log(`Solicitud recibida: ${req.method} ${req.url}`);
+    next(); //pasa el control al siguiente middleware o ruta
+}
+
+app.use(loggerMiddleware);
+
+
 //Middleare para parsear JSON
 app.use(express.json());
 
@@ -21,7 +30,7 @@ app.use('/api',superHeroRoutes);
 
 //Ruta principal
 app.get('/', function (req, res) {
-  res.send('¡Curso de Nodejs  2026 - Sprint 2 Trabajo Práctico 3!');
+  res.send('¡Curso de Nodejs  2026 - Sprint 3 Trabajo Práctico 1!');
 });
 
 //Manejo de errores para rutas no encontradas
